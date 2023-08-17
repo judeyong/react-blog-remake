ee24cdd6c8d0c0d89b57f303f464f253e740e8c5
jude <su2x123@gmail.com>
2023-08-17 22:28:25 +0900

    readme_sidedrawer-edit

--- src/shared/navigations/SideDrawer.js
+++ src/shared/navigations/SideDrawer.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import ReactDom from 'react-dom'
 import { CSSTransition } from "react-transition-group";
 
 import './SideDrawer.css';
 
 const SideDrawer = (props) => {
+  const nodeRef = useRef(null);
+
   const content = (
     <CSSTransition
       in={props.show}
       timeout={200}
       classNames="slide-in-left"
+      nodeRef={nodeRef}
       mountOnEnter
       unmountOnExit
     >
-      <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>
+      <aside ref={nodeRef} className="side-drawer" onClick={props.onClick}>{props.children}</aside>
     </CSSTransition>
   )
   return ReactDom.createPortal(content, document.getElementById('drawer-hook'));
